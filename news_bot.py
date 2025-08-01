945f274e8c93ad9027561b344ed6463c3c828664
Muhammad jabir <123949748+Muhummadjabir@users.noreply.github.com>
2025-08-01 17:37:56 +0100

    Update news_bot.py

--- news_bot.py
+++ news_bot.py
@@ -1,16 +1,5 @@
-from telegram import Bot, Update
-from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
+import time
 
-BOT_TOKEN = "123456789:AAABCDeFGhiJKLmnoPQRstuVWxyZ"  # replace with your full real token
-
-def reply_hello(update: Update, context: CallbackContext):
-    update.message.reply_text("Hello World")
-
-updater = Updater(token=BOT_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_hello))
-
-print("Bot started...")
-updater.start_polling()
-updater.idle()
+while True:
+    print("Hello")
+    time.sleep(10)
